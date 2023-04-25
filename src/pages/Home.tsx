adbc37247a8f655e70f0004fd10d5cceaab2696d
Sandro Vendeth <sandrohenrique92@outlook.com>
2023-04-25 14:05:46 -0300

    full aplication

---
+++ src/pages/Home.tsx
@@ -0,0 +1,104 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, TextInput, FlatList, } from 'react-native';
+import { Button } from '../components/Button';
+import { SkillCard } from '../components/SkillCard';
+
+interface SkillData {
+  id: string;
+  name: string;
+}
+
+export function Home() {
+  const [newSkill, setNewSkill] = useState('');
+  const [mySkills, setMySkills] = useState<SkillData[]>([]);
+  const [greeting, setGreeting] = useState('');
+
+  function handleAddNewSkill() {
+    const data = {
+        id: String(new Date().getTime()),
+        name: newSkill
+    }
+    setMySkills((oldState) => [...oldState, data]);
+  }
+
+  function handleRemoveSkill(id: string) {
+    setMySkills(oldState => oldState.filter(
+      skill => skill.id !== id
+    ));
+
+  }
+
+  useEffect(() => {
+    const currentHour = new Date().getHours();
+
+    if (currentHour < 12 ) {;
+      setGreeting('Good Morning!')
+    } else if (currentHour >= 12 && currentHour < 18) {
+      setGreeting('Good Afternoon!');
+    } else {  
+      setGreeting('Good Night!');
+    }
+  }, [mySkills]);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome, Sandro</Text>
+      <Text style={styles.greeting}>
+        { greeting }
+        </Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="New Skill"
+        placeholderTextColor="#666"
+        onChangeText={setNewSkill}
+      />
+
+      <Button 
+      onPress={handleAddNewSkill}
+      title="Add" />
+
+      <Text style={[styles.title, { marginVertical: 50 }]}>
+        My Skills
+      </Text>
+
+      <FlatList 
+        data={mySkills}
+        keyExtractor={item => item.id}
+        renderItem={({ item }) => (
+          <SkillCard 
+          skill={item.name}
+          onPress={() => handleRemoveSkill(item.id)}
+           />
+        )}
+
+      />   
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#121015",
+    paddingHorizontal: 30,
+    paddingVertical: 70,
+  },
+  title: {
+    color: "#fff",
+    fontSize: 24,
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: "#1F1e25",
+    color: "#fff",
+    fontSize: 18,
+    padding: 15,
+    marginTop: 30,
+    borderRadius: 7,
+  },
+  greeting: {
+    color: "#FFF"
+  }
+});
